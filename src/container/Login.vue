f679e53f7b9226abbc7b6b2a49e85c05906d3aa0
vidahaha <937634115@qq.com>
2018-04-18 18:02:46 +0800

    框架提交

---
+++ src/container/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login-wrap">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

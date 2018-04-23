9c074aafdbc083366333e83c9666adda529a1dc1
vidahaha <937634115@qq.com>
2018-04-23 22:31:07 +0800

    增加登陆组件

---
+++ src/components/LoginModal.vue
@@ -0,0 +1,112 @@
+<template>
+    <transition name="LoginModal">
+        <div class="modal-wrap" v-if="LoginModalPop">
+            <div class="mask" @click="close"></div>
+            <el-form class="content" :modal="form" label-width="80px" label-suffix=":">
+                <i class="close el-icon-close" @click="close"></i>
+                <el-form-item label="身份">
+                    <el-radio-group v-model="form.type" size="medium">
+                        <el-radio border label="学生"></el-radio>
+                        <el-radio border label="老师"></el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="姓名">
+                    <el-input placeholder="请输入姓名" prefix-icon="el-icon-star-off" v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="学号">
+                    <el-input placeholder="请输入学号" prefix-icon="el-icon-star-off" v-model="form.id"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input placeholder="请输入密码" prefix-icon="el-icon-star-off" v-model="form.password" type="password"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">立即登录</el-button>
+                    <el-button @click="close">取消</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </transition>
+</template>
+
+<script>
+    export default {
+        name: "LoginModal",
+        props:['LoginModalPop'],
+        data() {
+            return {
+                form: {
+                    type: 0, // 0学生1老师
+                    name: '',
+                    id: '',
+                    password: ''
+                }
+            };
+        },
+
+        methods: {
+            close () {
+                this.$emit("update:LoginModalPop", false)
+            },
+            onSubmit () {
+
+            }
+        }
+    };
+</script>
+
+<style scoped lang="less">
+    .LoginModal-enter-active,
+    .LoginModal-leave-active {
+        transition: opacity 0.5s;
+    }
+
+    .LoginModal-enter,
+    .LoginModal-leave-to {
+        opacity: 0;
+    }
+
+    .modal-wrap {
+        width: 100%;
+    }
+
+    .mask {
+        position: absolute;
+        height: 100%;
+        background: #303133;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        opacity: 0.8;
+    }
+
+    .content {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: #fff;
+        width: 500px;
+        height: 360px;
+        border: 1px solid #666;
+        border-radius: 20px;
+        padding: 30px;
+        box-sizing: border-box;
+        .close {
+            position: absolute;
+            font-size: 36px;
+            right: 10px;
+            top: 10px;
+            cursor: pointer;
+            &:hover {
+                animation: rotateIn 1s ease forwards;
+                color: #627d97;
+            }
+        }
+        .el-form-item {
+            text-align: left;
+            margin-top: 10px;
+            .el-input {}
+        }
+    }
+</style>
--- src/container/Login.vue
+++ src/container/Login.vue
@@ -1,31 +1,206 @@
 <template>
-  <div class="login-wrap">
-    <el-container>
-      <el-header>Header</el-header>
-      <el-main>Main</el-main>
-    </el-container>
-  </div>
+    <div class="login-wrap">
+        <el-container>
+            <el-header height="84px">
+                <div class="logo"></div>
+                <h1>华中农业大学社会实践管理系统</h1>
+                <el-button type="primary" class="login" @click="login">登录</el-button>
+            </el-header>
+            <el-main>
+                <div class="background">
+                    <div class="slogan"></div>
+                    <el-button type="primary" class="reg" @click="reg">
+                        立即注册！
+                        <i class="el-icon-arrow-right"></i>
+                    </el-button>
+                    <el-row class="options">
+                        <el-col :span="6">
+                            <div class="showTask content">
+                                <p class="title">项目相关内容公示</p>
+                                <p class="info">
+                                    各项目答辩时间，地点，经费报销，成绩公布
+                                </p>
+                                <div class="more">
+                                    <i class="el-icon-info"></i>
+                                    <a>点击此处查看</a>
+                                </div>
+                            </div>
+                        </el-col>
+                        <el-col :span="6">
+                            <div class="showMyTask content">
+                                <p class="title">项目相关内容公示</p>
+                                <p class="info">
+                                    各项目答辩时间，地点，经费报销，成绩公布
+                                </p>
+                                <div class="more">
+                                    <i class="el-icon-info"></i>
+                                    <a>点击此处查看</a>
+                                </div>
+                            </div>
+                        </el-col>
+                        <el-col :span="6">
+                            <div class="signTask content">
+                                <p class="title">项目相关内容公示</p>
+                                <p class="info">
+                                    各项目答辩时间，地点，经费报销，成绩公布
+                                </p>
+                                <div class="more">
+                                    <i class="el-icon-info"></i>
+                                    <a>点击此处查看</a>
+                                </div>
+                            </div>
+                        </el-col>
+                        <el-col :span="6">
+                            <div class="find content">
+                                <p class="title">项目相关内容公示</p>
+                                <p class="info">
+                                    各项目答辩时间，地点，经费报销，成绩公布
+                                </p>
+                                <div class="more">
+                                    <i class="el-icon-info"></i>
+                                    <a>点击此处查看</a>
+                                </div>
+                            </div>
+                        </el-col>
+                    </el-row>
+                </div>
+            </el-main>
+        </el-container>
+        <LoginModal :LoginModalPop.sync="LoginModalPop"></LoginModal>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'login',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
+    import LoginModal from "@/components/LoginModal";
 
-  mounted () {
-    this.axios.get('http://localhost:7001/login').then(function(res){
-      console.log(res.data)
-    }).catch(function(err){
-      console.log(err)
-    });
-  }
-}
+    export default {
+        name: "login",
+        data() {
+            return {
+                LoginModalPop: false,
+                RegModalPop: false
+            };
+        },
+
+        methods: {
+            login() {
+                this.LoginModalPop = true;
+            },
+
+            reg() {
+                this.RegModalPop = true;
+            }
+        },
+
+        components: {
+            LoginModal
+        },
+
+        mounted() {
+            this.axios
+                .get("http://localhost:7001/login")
+                .then(function(res) {
+                    console.log(res.data);
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
+        }
+    };
 </script>
 
-<style scoped>
+<style scoped lang="less">
+    .el-container {
+        font-size: 16px;
+        font-family: "Microsoft YaHei";
+    }
 
+    .el-header {
+        background: #f5f7fad1;
+        box-shadow: inset #c4c7ce 2px -4px 4px 0px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .logo {
+            background: url("/static/image/logo.png") no-repeat;
+            height: 67px;
+            width: 67px;
+            background-size: cover;
+        }
+        h1 {
+            color: #18157c;
+            letter-spacing: 3px;
+            font-weight: 400;
+        }
+        .login {}
+    }
+
+    .el-main {
+        padding: 0px;
+        .background {
+            background: url("/static/image/back.jpg") no-repeat;
+            width: 100%;
+            height: 900px;
+            background-position: center;
+            background-size: cover;
+            position: relative;
+            padding-top: 1px;
+        }
+        .slogan {
+            margin-top: 80px;
+            background: url("/static/image/slogan.png") no-repeat;
+            width: 980px;
+            height: 200px;
+            background-size: cover;
+            margin: 80px auto 60px;
+        }
+        .reg {
+            font-size: 20px;
+            letter-spacing: 3px;
+        }
+        .options {
+            width: 100%;
+            position: absolute;
+            bottom: 0;
+            .el-col {
+                .content {
+                    height: 280px;
+                    padding: 40px;
+                    box-sizing: border-box;
+                    .title {
+                        font-size: 25px;
+                        color: #fff;
+                        font-weight: 900;
+                    }
+                    .info {
+                        font-size: 18px;
+                        color: #fff;
+                        text-align: left;
+                        margin-top: 30px;
+                    }
+                    .more {
+                        font-size: 16px;
+                        color: #fff;
+                        margin-top: 45px;
+                    }
+                }
+            }
+            .showTask {
+                background-color: rgb(24, 21, 124);
+                opacity: 0.9;
+            }
+            .showMyTask {
+                background-color: rgb(77, 130, 239);
+                opacity: 0.9;
+            }
+            .signTask {
+                background-color: rgb(172, 213, 161);
+                opacity: 0.9;
+            }
+            .find {
+                background-color: #bfb348;
+                opacity: 0.9;
+            }
+        }
+    }
 </style>

7d286e915adfb2dc946031ecbefdcfb79f4403ed
vidahaha <937634115@qq.com>
2018-04-27 00:02:41 +0800

    完成apply

--- src/container/Apply.vue
+++ src/container/Apply.vue
@@ -3,49 +3,91 @@
         <el-container>
             <Header :LoginModalPop.sync="LoginModalPop" :isLogin.sync="isLogin" :name.sync="name"></Header>
             <el-main>
-                <div class="background">
-                    <el-form ref="form" :model="form" label-width="80px">
-                        <el-form-item label="活动名称">
-                            <el-input v-model="form.name"></el-input>
-                        </el-form-item>
-                        <el-form-item label="活动区域">
-                            <el-select v-model="form.region" placeholder="请选择活动区域">
-                                <el-option label="区域一" value="shanghai"></el-option>
-                                <el-option label="区域二" value="beijing"></el-option>
-                            </el-select>
-                        </el-form-item>
-                        <el-form-item label="活动时间">
-                            <el-col :span="11">
-                                <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-                            </el-col>
-                            <el-col class="line" :span="2">-</el-col>
-                            <el-col :span="11">
-                                <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-                            </el-col>
-                        </el-form-item>
-                        <el-form-item label="即时配送">
-                            <el-switch v-model="form.delivery"></el-switch>
-                        </el-form-item>
-                        <el-form-item label="活动性质">
-                            <el-checkbox-group v-model="form.type">
-                                <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                                <el-checkbox label="地推活动" name="type"></el-checkbox>
-                                <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                                <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-                            </el-checkbox-group>
-                        </el-form-item>
-                        <el-form-item label="特殊资源">
-                            <el-radio-group v-model="form.resource">
-                                <el-radio label="线上品牌商赞助"></el-radio>
-                                <el-radio label="线下场地免费"></el-radio>
+                <div class="background apply">
+                    <el-form ref="form" :model="form" label-width="80px" align="left" label-suffix=":">
+                        <p>#团队活动概况</p>
+                        <el-form-item label="项目类型">
+                            <el-radio-group v-model="form.type">
+                                <el-radio border label="校级"></el-radio>
+                                <el-radio border label="院级"></el-radio>
                             </el-radio-group>
                         </el-form-item>
-                        <el-form-item label="活动形式">
-                            <el-input type="textarea" v-model="form.desc"></el-input>
+                        <el-form-item label="主题">
+                            <el-input type="input" v-model="form.task_full_name"></el-input>
                         </el-form-item>
+                        <el-form-item label="时间" class="short">
+                            <el-input type="input" v-model="form.task_time"></el-input>
+                        </el-form-item>
+                        <el-form-item label="地点" class="short">
+                            <el-input type="input" v-model="form.task_place"></el-input>
+                        </el-form-item>
+                        <el-form-item label="团队名称">
+                            <el-input type="input" v-model="form.team_name"></el-input>
+                        </el-form-item>
+                        <el-form-item label="参与人数" class="short">
+                            <el-input type="input" v-model="form.task_num"></el-input>
+                        </el-form-item>
+                    </el-form>
+                    <el-form ref="form" :model="form" label-width="80px" align="left" label-suffix=":" size="small">
+                        <p>#负责人信息</p>
+                        <div class="lead">
+                            <el-form-item label="姓名">
+                                <el-input type="input" v-model="form.task_lead_name"></el-input>
+                            </el-form-item>
+                            <el-form-item label="性别">
+                                <el-input type="input" v-model="form.task_lead_sex"></el-input>
+                            </el-form-item>
+                            <el-form-item label="政治面貌">
+                                <el-input type="input" v-model="form.task_lead_party"></el-input>
+                            </el-form-item>
+                            <el-form-item label="学院">
+                                <el-input type="input" v-model="form.task_lead_college"></el-input>
+                            </el-form-item>
+                            <el-form-item label="专业">
+                                <el-input type="input" v-model="form.task_lead_major"></el-input>
+                            </el-form-item>
+                            <el-form-item label="年级">
+                                <el-input type="input" v-model="form.task_lead_grade"></el-input>
+                            </el-form-item>
+                            <el-form-item label="联系电话">
+                                <el-input type="input" v-model="form.task_lead_phone"></el-input>
+                            </el-form-item>
+                            <el-form-item label="邮箱">
+                                <el-input type="input" v-model="form.task_lead_email"></el-input>
+                            </el-form-item>
+                            <el-form-item label="QQ号码">
+                                <el-input type="input" v-model="form.task_lead_qq"></el-input>
+                            </el-form-item>
+                        </div>
+                    </el-form>
+                    <el-form class="memberForm" ref="form" :model="form" label-width="80px" align="left" label-suffix=":" size="small">
+                        <p>#成员信息</p>
+                        <div class="member" v-for="(member, index) in form.members" :key="index">
+                            <el-form-item label="姓名">
+                                <el-input type="input" v-model="member.task_member_name"></el-input>
+                            </el-form-item>
+                            <el-form-item label="学院">
+                                <el-input type="input" v-model="member.task_member_college"></el-input>
+                            </el-form-item>
+                            <el-form-item label="专业">
+                                <el-input type="input" v-model="member.task_member_major"></el-input>
+                            </el-form-item>
+                            <el-form-item label="年级">
+                                <el-input type="input" v-model="member.task_member_grade"></el-input>
+                            </el-form-item>
+                            <el-form-item label="联系电话">
+                                <el-input type="input" v-model="member.task_member_phone"></el-input>
+                            </el-form-item>
+                        </div>
                         <el-form-item>
-                            <el-button type="primary" @click="onSubmit">立即创建</el-button>
-                            <el-button>取消</el-button>
+                            <el-button type="primary" @click="addMember">新增成员</el-button>
+                            <el-button type="primary" @click="removeMember" v-if="form.members.length>1">删除成员</el-button>
+                        </el-form-item>
+                    </el-form>
+                    <el-form class="btnForm" ref="form">
+                        <el-form-item>
+                            <el-button type="primary" @click="onSubmit">立即申请</el-button>
+                            <el-button type="warning" @click="onSubmit">重新填写</el-button>
                         </el-form-item>
                     </el-form>
                 </div>
@@ -71,14 +113,16 @@
                 LoginModalPop: false,
                 RegModalPop: false,
                 form: {
-                    name: '',
-                    region: '',
-                    date1: '',
-                    date2: '',
-                    delivery: false,
-                    type: [],
-                    resource: '',
-                    desc: ''
+                    type: '',
+                    task_full_name: '',
+                    task_time: '',
+                    members: [{
+                        task_member_name: '',
+                        task_member_college: '',
+                        task_member_major: '',
+                        task_member_grade: '',
+                        task_member_phone: '',
+                    }],
                 }
             };
         },
@@ -96,6 +140,20 @@
 
             onSubmit() {
                 console.log('submit!');
+            },
+
+            addMember() {
+                this.form.members.push({
+                    task_member_name: '',
+                    task_member_college: '',
+                    task_member_major: '',
+                    task_member_grade: '',
+                    task_member_phone: '',
+                });
+            },
+
+            removeMember() {
+                this.form.members.pop();
             }
         },
 
@@ -139,22 +197,67 @@
         .background {
             background: url("/static/image/back_1.png") no-repeat;
             width: 100%;
-            height: 900px;
             background-position: center;
             background-size: cover;
             position: relative;
             padding-top: 1px;
             overflow: hidden; // opacity: .5;
         }
-        .el-table {
+        .el-form {
             color: #303133;
-            width: 90%;
             margin: 60px auto;
             border: 2px solid #999;
             border-radius: 10px;
-            .thead {
-                background-color: #edfffe;
+            width: 70%;
+            background: rgba(200, 206, 239, 0.5);
+            padding: 30px;
+            &.memberForm {
+                width: 80%;
             }
+            &.btnForm {
+                width: 230px;
+                padding: 10px;
+                .el-form-item {
+                    margin: 0;
+                }
+            }
+            p {
+                text-align: center;
+                font-size: 20px;
+                font-weight: bold;
+                margin-bottom: 15px;
+                margin-top: -10px;
+            }
+            .short {
+                width: 200px;
+                display: inline-block;
+            }
+            .medium {
+                width: 400px;
+                display: inline-block;
+            }
+        }
+        .lead {
+            display: flex;
+            flex-flow: wrap;
+            justify-content: space-around;
+        }
+        .member {
+            display: flex;
+            flex-flow: nowrap;
+            justify-content: space-around;
+        }
+    }
+</style>
+
+<style lang="less">
+    .apply {
+        .el-input__inner {
+            background: rgba(255, 255, 255, .5);
+        }
+        .el-form-item__label {
+            font-size: 16px;
+            color: #303133;
         }
     }
 </style>
